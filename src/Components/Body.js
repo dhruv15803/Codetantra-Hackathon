f81a834e14a276bf08e79749eca49751275fac74
dhruv15803 <122736571+dhruv15803@users.noreply.github.com>
2023-09-15 12:43:21 +0530

    newCommit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -10,6 +10,7 @@ export default function Body(props) {
             <p>Find the scholarships according to your country,Degree and much more</p>
             <button className="bodyBtn">Looking for scholarships</button>
         </div>
+        <img src="https://cdn.discordapp.com/attachments/1151781933748064337/1152137337673895966/istockphoto-transformed.jpeg" alt="" />
     </div>
     <div className="popularCountries">
             <h2>Most popular countries</h2>
@@ -17,17 +18,17 @@ export default function Body(props) {
                 <div className="countryItem">
                     <img src="https://cdn.discordapp.com/attachments/1151781933748064337/1152124137469394964/uk.webp" alt="" />
                     <p>United kingdom</p>
-                    <button className="country-btn">See scholarships</button>
+                    <button className="country-btn" onClick={()=>window.location='/uk'}>See scholarships</button>
                 </div>
                 <div className="countryItem">
                     <img src="https://cdn.discordapp.com/attachments/1151781933748064337/1152123837635383317/usa.jpg" alt="" />
                     <p>United states</p>
-                    <button className="country-btn">See scholarships</button>
+                    <button className="country-btn" onClick={()=>window.location='/us'}>See scholarships</button>
                 </div>
                 <div className="countryItem">
                     <img src="https://cdn.discordapp.com/attachments/1151781933748064337/1152123501688393768/canada.jpg" alt="" />
                     <p>Canada</p>
-                    <button className="country-btn">See scholarships</button>
+                    <button className="country-btn" onClick={()=>window.location='/Canada'}>See scholarships</button>
                 </div>
             </div>
         </div>
