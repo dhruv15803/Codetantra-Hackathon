27042071c7289349bc12a3da4d856e99e7fe7d8a
dhruv15803 <122736571+dhruv15803@users.noreply.github.com>
2023-09-15 13:50:40 +0530

    another one

--- src/Components/CountryScholarships.js
+++ src/Components/CountryScholarships.js
@@ -13,21 +13,40 @@ export default function CountryScholarships(props) {
         }
     },[props.country]);
 
-    console.log(scholarships);
+    const applyForScholarship = (index)=>{
+        console.log(`Applied for ${scholarships[index].title} `)
+    }
 
   return (
     <>
     <div className="country-scholarships-parentContainer">
-        {scholarships.map((item)=>{
+        {scholarships.map((item,index)=>{
             return <div className="country-scholarships-Container">
-            <h2>{item.title}</h2>
-            <p>{item.degrees}</p>
-            <p>{item.funds}</p>
-            <p>{item.date}</p>
-            <p>{item.location}</p>
+            <div className="scholarship-div">
+                <i class="fa-solid fa-building-columns"></i>
+                <h2>{item.title}</h2>
+            </div>
+            <div className="scholarship-div">
+                <i class="fa-solid fa-scroll"></i>
+                <p>{`Degrees offered: ${item.degrees}`}</p>
+            </div>
+            <div className="scholarship-div">
+                <i class="fa-solid fa-sack-dollar"></i>
+                <p>{item.funds}</p>
+            </div>
+            <div className='scholarship-div'>
+                <i class="fa-solid fa-calendar-days"></i>
+                <p>{`Available till: ${item.date}`}</p>
+            </div>
+            <div className='scholarship-div'>
+                <i class="fa-solid fa-location-dot"></i>
+                <p>{`Country:- ${item.location}`}</p>
+            </div>
+            <button className="country-scholarships-button" onClick={()=>applyForScholarship(index)}>Apply</button>
         </div>
         })}
     </div>
     </>
   )
 }
+
